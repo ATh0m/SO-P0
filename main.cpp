dce52d3473ca1e88964ac88ee48fabe6afa54357
Tomasz Nanowski <tomasz.nanowski@gmail.com>
2016-10-28 15:06:30 +0200

    Added main.cpp

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <thread>
+#include <sstream>
+
+void foo(int x)
+{
+    std::cout << "Child #" + std::to_string(x) + "\n";
+}
+
+#define N 10
+
+int main()
+{
+    std::thread threads[N];
+
+    for (int i = 0; i < N; i++)
+        threads[i] = std::thread(foo, i);
+
+    for (int i = 0; i < N; i++)
+        threads[i].join();
+
+    std::cout << "Parent" << std::endl;
+    
+    return 0;
+}
